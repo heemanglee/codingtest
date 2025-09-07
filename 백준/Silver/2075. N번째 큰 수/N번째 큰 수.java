31c0b6516db17e64fbe56a8fd169a27d0cb8008a
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-09-08 08:29:02 +0900

    [Silver III] Title: N번째 큰 수, Time: 1748 ms, Memory: 343752 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            String[] inputs = br.readLine().split(" ");
+            for (int j = 0; j < N; j++) {
+                list.add(Integer.parseInt(inputs[j]));
+            }
+        }
+
+        Collections.sort(list, Collections.reverseOrder());
+        System.out.println(list.get(N - 1));
+    }
+}
