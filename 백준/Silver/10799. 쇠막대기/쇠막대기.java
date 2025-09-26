cfa411e5f961cc1ed63c4831952a1a4f7c57ae9b
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-09-26 21:38:00 +0900

    [Silver II] Title: 쇠막대기, Time: 128 ms, Memory: 15748 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,29 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         char[] cArr = br.readLine().toCharArray();
-        Stack<Character> stack = new Stack<>();
 
         int sum = 0;
-        char prev = '(';
-        for (char c : cArr) {
-            if (c == '(') {
-                stack.push(c);
+        int open = 0;
+        for (int i = 0; i < cArr.length; i++) {
+            if (cArr[i] == '(') {
+                open++;
             } else {
-                stack.pop();
-                if (prev == '(') { // 막대기 사이 레이저
-                    sum += stack.size();
-                } else { // 막대디 끝
-                    sum += 1;
-                }
+                open--;
+                sum += cArr[i - 1] == '(' ? open : 1;
             }
-            prev = c;
         }
 
         System.out.println(sum);
