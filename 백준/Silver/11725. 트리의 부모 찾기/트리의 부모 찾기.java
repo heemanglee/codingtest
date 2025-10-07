dca22d08577e32de0c9b6a141afcb065afc407fa
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-10-07 19:52:47 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 520 ms, Memory: 59268 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,11 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -18,38 +14,37 @@ public class Main {
         }
 
         StringTokenizer st;
-        for (int i = 1; i < N; i++) {
+        for (int i = 0; i < N - 1; i++) {
             st = new StringTokenizer(br.readLine());
-            int node1 = Integer.parseInt(st.nextToken());
-            int node2 = Integer.parseInt(st.nextToken());
-            graph.get(node1).add(node2);
-            graph.get(node2).add(node1);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph.get(a).add(b);
+            graph.get(b).add(a);
         }
 
         boolean[] visited = new boolean[N + 1];
         int[] result = new int[N + 1];
+        visited[1] = true;
 
         Queue<Integer> que = new ArrayDeque<>();
         que.offer(1);
-        visited[1] = true;
 
         while (!que.isEmpty()) {
             int parent = que.poll();
             for (int child : graph.get(parent)) {
-                if (visited[child]) {
-                    continue;
-                }
-                que.offer(child);
+                if (visited[child]) continue;
                 result[child] = parent;
+                que.offer(child);
                 visited[child] = true;
             }
         }
 
         StringBuilder sb = new StringBuilder();
         for (int i = 2; i <= N; i++) {
-            sb.append(result[i] + "\n");
+            sb.append(result[i]).append("\n");
         }
-
         System.out.println(sb);
+
     }
 }
