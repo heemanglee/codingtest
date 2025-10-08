5dff63f2c1b30cbf83ff8579de5ae58da85da9ca
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-10-08 10:27:33 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 504 ms, Memory: 53252 KB -BaekjoonHub

---
+++ 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        TreeSet<Pair> pairSet = new TreeSet<>();
+        Map<Integer, Pair> pairMap = new HashMap<>();
+
+        StringTokenizer st;
+        int p, l, x;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            p = Integer.parseInt(st.nextToken());
+            l = Integer.parseInt(st.nextToken());
+            Pair pair = new Pair(p, l);
+            pairSet.add(pair);
+            pairMap.put(p, pair);
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            String order = st.nextToken();
+
+            switch (order) {
+                case "add":
+                    p = Integer.parseInt(st.nextToken());
+                    l = Integer.parseInt(st.nextToken());
+                    Pair pair = new Pair(p, l);
+                    pairSet.add(pair);
+                    pairMap.put(p, pair);
+                    break;
+                case "recommend":
+                    x = Integer.parseInt(st.nextToken());
+                    sb.append(x == -1 ? pairSet.first().p : pairSet.last().p).append("\n");
+                    break;
+                default:
+                    p = Integer.parseInt(st.nextToken());
+                    pairSet.remove(pairMap.get(p));
+                    pairMap.remove(p);
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    static class Pair implements Comparable<Pair> {
+        int p; // 문제 번호
+        int l; // 난이도
+
+        public Pair(int p, int l) {
+            this.p = p;
+            this.l = l;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            if (this.l == o.l) {
+                return this.p - o.p;
+            }
+            return this.l - o.l;
+        }
+    }
+}
