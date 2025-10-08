f08f45c264738c48b19324e9f8da73eb7f8766bf
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-10-08 11:28:22 +0900

    [Silver IV] Title: 지뢰 찾기, Time: 96 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/4396. 지뢰 찾기/지뢰 찾기.java
+++ 백준/Silver/4396. 지뢰 찾기/지뢰 찾기.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.Arrays;
 
 public class Main {
 
@@ -11,70 +10,53 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine()); // N x N
-        char[][] mines = new char[N][N];
-        Queue<int[]> que = new ArrayDeque<>();
 
         // 지뢰 좌표 저장
-        for (int i = 0; i < N; i++) {
-            char[] arr = br.readLine().toCharArray();
-            for (int j = 0; j < N; j++) {
-                mines[i][j] = arr[j];
-            }
-        }
+        char[][] mine = new char[N][N];
+        for (int y = 0; y < N; y++) mine[y] = br.readLine().toCharArray();
 
-        // 사용자가 선택한 좌표 저장
-        for (int y = 0; y < N; y++) {
-            char[] arr = br.readLine().toCharArray();
-            for (int x = 0; x < N; x++) {
-                if (arr[x] == 'x') {
-                    que.add(new int[]{y, x});
-                }
-            }
-        }
+        // 현재 좌표 상태 저장
+        char[][] open = new char[N][N];
+        for (int y = 0; y < N; y++) open[y] = br.readLine().toCharArray();
 
-        String[][] result = new String[N][N];
+        // 출력할 배열
+        char[][] result = new char[N][N];
+        for (int y = 0; y < N; y++) Arrays.fill(result[y], '.');
+
         boolean isMeetMine = false;
 
-        while (!que.isEmpty()) {
-            int[] pos = que.poll();
-            int x = pos[1];
-            int y = pos[0];
-
-            // 현재 자리에 지뢰가 있을 경우
-            if (mines[y][x] == '*') {
-                isMeetMine = true;
-                continue;
-            }
-
-            int mineCount = 0;
-            for (int d = 0; d < 8; d++) {
-                int nx = x + dx[d];
-                int ny = y + dy[d];
-                if (isNotOutOfRange(nx, ny, N)) {
-                    if (mines[ny][nx] == '*') {
-                        mineCount++;
-                    }
-                }
-            }
-            result[y][x] = mineCount + "";
-        }
-
         for (int y = 0; y < N; y++) {
             for (int x = 0; x < N; x++) {
-                if (result[y][x] == null) {
-                    if (isMeetMine && mines[y][x] == '*') {
-                        result[y][x] = "*";
+                if (open[y][x] == 'x') {
+                    if (mine[y][x] == '*') {
+                        isMeetMine = true;
                     } else {
-                        result[y][x] = ".";
+                        int cnt = 0;
+                        for (int d = 0; d < 8; d++) {
+                            int nx = x + dx[d];
+                            int ny = y + dy[d];
+                            if (isNotOutOfRange(nx, ny, N) && mine[ny][nx] == '*') {
+                                cnt++;
+                            }
+                        }
+                        result[y][x] = (char) ('0' + cnt);
                     }
                 }
-                sb.append(result[y][x]);
             }
-            sb.append("\n");
         }
+
+        if (isMeetMine) {
+            for (int y = 0; y < N; y++) {
+                for (int x = 0; x < N; x++) {
+                    if (mine[y][x] == '*') result[y][x] = '*';
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int y = 0; y < N; y++) sb.append(result[y]).append("\n");
 
         System.out.println(sb);
     }
