d6eca4f4512b9a83a8c0bc914cba9ef6267c284c
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-10-10 23:52:21 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 196 ms, Memory: 22696 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,7 +10,7 @@ public class Main {
 
         char[] arr = br.readLine().toCharArray();
 
-        Stack<Character> stack = new Stack<>();
+        Deque<Character> stack = new ArrayDeque<>();
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < arr.length; i++) {
@@ -39,7 +40,7 @@ public class Main {
         System.out.println(sb);
     }
 
-    private static void consume(Stack<Character> stack, StringBuilder sb) {
+    private static void consume(Deque<Character> stack, StringBuilder sb) {
         while (!stack.isEmpty()) {
             sb.append(stack.pop());
         }
