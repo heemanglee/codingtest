b76db0a45ddfa320cc4322d51c695cf9c000d240
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-10-11 20:35:03 +0900

    [Silver III] Title: 원상 복구 (small), Time: 324 ms, Memory: 58588 KB -BaekjoonHub

---
+++ 백준/Silver/22858. 원상 복구 （small）/원상 복구 （small）.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[] pArr;
+    static int[] dArr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        pArr = new int[N];
+        dArr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            pArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            dArr[i] = Integer.parseInt(st.nextToken()) - 1;
+        }
+
+        int[] tmp = new int[N];
+        while (M-- > 0) {
+            for (int i = 0; i < N; i++) {
+                int num = pArr[i];
+                int moveIdx = dArr[i];
+                tmp[moveIdx] = num;
+            }
+
+            pArr = tmp.clone();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(pArr[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
